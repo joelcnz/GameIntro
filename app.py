bf8be937d13af11775dd3d8c326a98e50c34b7df
Joel Christensen <joelcnz@gmail.com>
2018-03-28 15:52:53 +1300

    keys, text and colour s
    Added pressing keys (if bounce is false). And press space to change the text.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import sys, pygame
 from pygame.locals import *
 from ball import *
+from base import *
 
 def main():
 	pygame.init()
+	pygame.display.set_caption('Game Intro')
 
 	size = width, height = 320, 240
-	black = 0, 0, 0
 
 	screen = pygame.display.set_mode(size)
 
@@ -14,16 +15,41 @@ def main():
 	clock = pygame.time.Clock()
 	
 	character = Ball(width, height)
-		
+	character.setBounce(True)
+
+	# Display some text
+	font = pygame.font.Font(None, 36)
+	text = font.render("Press [Space]", 1, (0, 0, 0))
+	textpos = text.get_rect()
+
 	done = False
 	while not done:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: done = True
+			
+			if character.bounce == False:
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_UP: character.dir[1] = -character.speed[1]
+					if event.key == pygame.K_DOWN: character.dir[1] = character.speed[1]
+					if event.key == pygame.K_LEFT: character.dir[0] = -character.speed[0]
+					if event.key == pygame.K_RIGHT: character.dir[0] = character.speed[0]
+
+				if event.type == KEYUP:
+					if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+						character.dir[1] = 0
+					if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+						character.dir[0] = 0
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_SPACE:
+					text = font.render("Space Pressed", 1, (0, 0, 0))
 
 		character.move()
 		
-		screen.fill(black)
+		screen.fill(GREEN)
 		character.draw(screen)
+		screen.blit(text, textpos)
+
 		pygame.display.flip()
 		clock.tick(60)
 	sys.exit()
--- ball.py
+++ ball.py
@@ -11,14 +11,29 @@ class Ball(pygame.sprite.Sprite):
 		self.ball = pygame.image.load("intro_ball.gif")
 		self.ballrect = self.ball.get_rect()
 		self.pos = [10,10]
+		self.dir = [0,0]
+		self.bounce = False
 
-	def move(self):
+	def setBounce(self, bounce):
+		self.bounce = bounce
+	
+	def moveBounce(self):
 		self.pos[0] += self.speed[0]
 		self.pos[1] += self.speed[1]
 		if self.pos[0] < 0 or self.pos[0] + self.ballrect.width > self.width:
 			self.speed[0] = -self.speed[0]
 		if self.pos[1] < 0 or self.pos[1] + self.ballrect.height > self.height:
 			self.speed[1] = -self.speed[1]
+
+	def userControl(self):
+		self.pos[0] += self.dir[0]
+		self.pos[1] += self.dir[1]
+
+	def move(self):
+		if self.bounce:
+			self.moveBounce()
+		if not self.bounce:
+			self.userControl()
 
 	def draw(self, screen):
 		screen.blit(self.ball, self.pos)
